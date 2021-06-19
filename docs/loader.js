fab3ef6da6c893a744a1bfdcb0b650b5bdc9c81e
jobscale <jobscale@users.noreply.github.com>
2021-06-19 11:19:59 +0900

    template loader updated

--- docs/loader.js
+++ docs/loader.js
@@ -1,14 +1,19 @@
-const action = () => {
-fetch('template.html')
-.then(res => res.text())
-.then(content => {
-  document.body.innerHTML += content;
-  const templates = document.querySelectorAll('div[id^=template-]');
-  templates.forEach(template => {
-    const inline = template.id.replace(/template/, 'inline');
-    const fragment = document.querySelector(`#${inline}`);
-    template.append(fragment.content);
-  });
-})
-};
-document.addEventListener('DOMContentLoaded', action);
+class Loader {
+  action() {
+    const templates = document.querySelectorAll('div[id^=template-]');
+    templates.forEach(template => {
+      const inline = template.id.replace(/template/, 'inline');
+      const fragment = document.querySelector(`#${inline}`);
+      template.append(fragment.content);
+    });
+  }
+  trigger() {
+    fetch('template.html')
+    .then(res => res.text())
+    .then(content => {
+      document.body.innerHTML += content;
+      this.action();
+    });
+  }
+}
+document.addEventListener('DOMContentLoaded', () => new Loader().trigger());
