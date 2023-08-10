081797efab6b21faa3ce07f7c4faeb8e6b1e45db
jobscale <jobscale@users.noreply.github.com>
2023-08-11 07:45:14 +0900

    dayjs added

--- docs/js/index.js
+++ docs/js/index.js
@@ -18,8 +18,6 @@ Vue.createApp({
   },
 
   mounted() {
-    logger.info('jsxjp');
-    this.singleton();
     this.start();
   },
 
@@ -30,24 +28,9 @@ Vue.createApp({
       html.classList.toggle('light-scheme');
     },
 
-    singleton() {
-      // eslint-disable-next-line func-names
-      Object(Date).prototype.toLocaleString = function () {
-        const dt = this;
-        const d = {
-          Y: dt.getFullYear(),
-          m: `0${dt.getMonth() + 1}`.slice(-2),
-          d: `0${dt.getDate()}`.slice(-2),
-          H: `0${dt.getHours()}`.slice(-2),
-          M: `0${dt.getMinutes()}`.slice(-2),
-          S: `0${dt.getSeconds()}`.slice(-2),
-        };
-        return `${d.Y}-${d.m}-${d.d} ${d.H}:${d.M}:${d.S}`;
-      };
-    },
-
     start() {
-      setTimeout(() => this.interval(), 0);
+      logger.info('Start jsx.jp');
+      setTimeout(() => this.interval(), 200);
     },
 
     action() {
@@ -94,7 +77,8 @@ Vue.createApp({
           const diff = Math.floor((Date.now() - serverTime.getTime()) / 100) / 10;
           this.actionText += ` ${diff}`;
         }
-        this.dateText = serverTime.toLocaleString();
+        const [date, time] = dayjs(serverTime).add(9, 'hour').toISOString().split(/[T.]/);
+        this.dateText = `${date} ${time}`;
         this.stack.unshift(span);
         this.updateSpan();
       })
@@ -107,7 +91,8 @@ Vue.createApp({
       if (this.busy) {
         if (this.busy === 1) {
           this.busyList.unshift(1);
-          this.busyTime = new Date().toLocaleTimeString();
+          const [date, time] = dayjs().add(9, 'hour').toISOString().split(/[T.]/);
+          this.busyTime = time;
         }
         this.busyList[0]++;
         this.busy++;
